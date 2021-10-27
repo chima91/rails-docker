425a03f0ff369892c90d5f408bd397ef8a66ee60
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-27 21:47:50 +0900

    replace with resource base routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  get 'boards/:id', to: 'boards#show'
-  post 'boards', to: 'boards#create'
+
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # get 'boards/:id', to: 'boards#show'
+  # post 'boards', to: 'boards#create'
+
+  resources :boards, only: [:index, :new, :create, :show]
 end
