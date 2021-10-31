faa7d11a360fead9f5de72c56175fcd0c52f4e9f
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-31 21:41:44 +0900

    create comments controller with create, destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'boards', to: 'boards#index'
