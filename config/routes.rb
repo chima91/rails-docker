5c6b489058b9c7573cb4555878ac5d1cd7a37a39
Kazuma NAGAI <itb4769we@gmail.com>
2021-11-05 21:56:45 +0900

    create routing for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
-  get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/me'
+  get 'mypage', to: 'users#me'
+  # get 'home/index'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get 'boards', to: 'boards#index'
@@ -15,6 +18,8 @@ Rails.application.routes.draw do
   # get 'comments/create'
   # get 'comments/destroy'
 
+  root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy]
+  resources :users, only: %i[new create]
 end
