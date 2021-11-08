526804eb470a2c9ae4ca7b6519156ce003abaaf0
Kazuma NAGAI <itb4769we@gmail.com>
2021-11-08 21:30:00 +0900

    add before_action of ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # protect_form_forgery with: :exception
+  before_action :current_user
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
